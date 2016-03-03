21fab22d66ec0df707ea8a3d49a502cf25ea9783
shinyrachel <rachel.kory@gmail.com>
2016-03-03 16:16:13 -0500

    complete

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      found_item = @@items.find { |i| i.name == item }
+      if !found_item.nil?
+        resp.write "#{found_item.name}: $#{found_item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
