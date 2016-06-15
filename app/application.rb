bc9fb709fa6287d6fdd71307f9ef84b9e6074867
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-06-15 21:01:01 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,25 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/songs"
-      resp.write
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if @@items.find {|x| x.name == item_name}
+        item = @@items.find {|x| x.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+
+    resp.finish
+
+
+
+
   end
 
 end
