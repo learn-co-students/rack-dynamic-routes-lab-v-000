dfead40df4bd42cd07d925848a6619b55ed4fe55
WilliamMena <williammena1991@gmail.com>
2016-08-12 17:38:04 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,27 @@
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-     #the link should return the price of the asked for item
+    #the link should return the price of the asked for item
     #if you don't have that item, return a 400 and an error message
     #if any other link is requested, send a 404 message
+    
     if req.path.match(/items/)
-       item_name = req.math.split("/item/").last
-       if Item.all.include?(item_name)
-        item = Item.all.find{|i| i.name == item_name}
+       item_name = req.path.split("/items/").last
+       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
         resp.status = 400
-        resp.write "We don't have that item."
+        resp.write "Item not found"
       end
      else
        resp.status = 404
+       resp.write "Route not found"
     end
+    resp.finish
    end
 
 end
