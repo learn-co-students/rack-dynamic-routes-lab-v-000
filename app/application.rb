39968e1fe8cf1b9ee21ac1690a0bad8696e98230
William Jones <enquiries@forestfed.com>
2016-08-29 19:27:26 +0000

    rack dynamic routes

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      if found_item = @@items.detect{|s| s.name == item}
+        resp.write "#{found_item.price.to_s}"
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
