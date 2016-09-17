2884fa13630e88f04555557569e529a599b9c4ef
Lani1321 <lani.berry13@gmail.com>
2016-09-17 16:38:30 -0400

    solution

--- app/item.rb
+++ app/item.rb
@@ -1,3 +1,7 @@
+# Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
+# If a user requests /items/<Item Name> it should return the price of that item
+# IF a user requests an item that you don't have, then return a 400 and an error message
+
 class Item
   attr_accessor :name, :price
 
