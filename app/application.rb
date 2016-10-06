c8129ba1b6864ef5beb6e0d991a54f73424857a6
David Kennell <dkennell998@hotmail.com>
2016-10-06 14:05:03 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,22 @@
 class Application
 
-  def call
+  def call(env)
 
     resp = Rack::Response.new
-    req = Rack.Require.new(env)
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       search_item = req.params["item"]
-      resp.write(search_item.price)
+      @@items. each do |item|
+        if item.name == search_item
+          return item.price
+        end
+      end
+      resp.status == 400
+      resp.write "Item not found."
     else
       resp.status = 404
-      resp.write "Item  not found"
+      resp.write "Route not found"
     end
     resp.finish
   end
