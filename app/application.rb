2ef1180ebf8c7758076b84cd6984a7da9abe8113
Kelsey Huse <kelseyhuse30@gmail.com>
2017-03-21 22:26:56 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Application
+
+	def call(env)
+		resp = Rack::Response.new
+		req = Rack::Request.new(env)
+
+		if req.path.match(/items/)
+			item_name = req.path.split("/items/").last
+
+			item = Item.all.find {|i| i.name == item_name }
+			
+			if item == nil
+				resp.status = 400
+				resp.write "Item not found"				
+			else
+				resp.write item.price
+			end
+
+		else
+			resp.write "Route not found"
+			resp.status = 404
+		end
+
+		resp.finish
+	end
+
+end
