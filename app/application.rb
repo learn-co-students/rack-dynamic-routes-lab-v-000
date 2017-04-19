e44435661f41b85c8a4998db642edd79dd74ef7c
Bryan Hou <bryan.hou94@gmail.com>
2017-04-19 19:46:06 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/\/items\//)
+      item_name = req.path.split("/items/")[-1]
+      item = Item.all.select { |item| item.name == item_name}
+
+      if item.size!=0
+        resp.write item.first.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
