ebd630e6a131b286a34e0cbb72ee26c4c06999d1
ETphonehomeslice <mk.etlinger@gmail.com>
2017-04-25 10:25:38 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Application
+
+  @@items = [Item.new("gala",5.23), Item.new("Oranges",2.43)]
+
+  def call(env)
+    response = Rack::Response.new
+    request = Rack::Request.new(env)
+
+    if request.path.match(/items/)
+      item_name = request.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        response.write item.price
+      else
+        response.status = 400
+        response.write "Item not found"
+      end
+    else
+      response.status = 404
+      response.write "Route not found"
+    end
+
+    response.finish
+  end
+
+  def item_in_stock(requested_item)
+    @@items.find {|item| item.name = requested_item}
+  end
+
+end
