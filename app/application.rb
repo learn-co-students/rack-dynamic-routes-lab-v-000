4265668156de527cfa4e5502776d723d4b341deb
Lia HUANG <lisa.yc.huang@gmail.com>
2017-05-01 11:04:33 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      item_selected = @@items.find{|s| s.name == item_name}
+
+      if @@items.include?(item_selected)
+        resp.write item_selected.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
