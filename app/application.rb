fd16d8575b8f72e08b134fb3cde25e52be251d77
Scott Wakeman <swakeman@ca.rr.com>
2017-06-25 05:00:10 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write item.price
+      end
+
+    elsif
+      resp.write "Route not found"
+      resp.status = 404
+
+    else
+      resp.write "Item not found"
+      resp.status = 400
+    end
+    resp.finish
+  end
+end
