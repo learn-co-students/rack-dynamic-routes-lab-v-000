bcb83e01a04b6caf1d4e01576ed80c2472a6834f
Scott Wakeman <swakeman@ca.rr.com>
2017-06-26 18:35:59 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -21,32 +21,3 @@ class Application
     resp.finish
   end
 end
-
-
-#class Application
-  #@@items = []
-  #def call(env)
-    #resp = Rack::Response.new
-    #req = Rack::Request.new(env)
-
-    #if req.path.!match(/items/)
-      #@@items.each do |item|
-        #resp.write "Route not found"
-        #resp.status = 404
-      #end
-
-    #elsif
-      #search_term = req.params["q"]
-
-      #if @@items.include(search_term)
-        #resp.status = 200
-
-      #else
-        #resp.write "Item Not Found"
-        #resp.status = 400
-
-      #end
-    #end
-    #resp.finish
-  #end
-#end
