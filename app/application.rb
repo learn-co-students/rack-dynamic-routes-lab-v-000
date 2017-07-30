b2a5b08bf047241c549327463df5ba571529d141
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-30 03:26:00 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,39 @@
+
+
+class Application
+
+  @@items = []
+
+  def self.items
+    @@items
+  end
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      req_item = req.path.split("/items/").last
+      # binding.pry
+      found_item = self.class.items.find do |item|
+        item.name == req_item
+      end
+# binding.pry
+      if found_item
+        resp.status = 200
+        resp.write found_item.price
+
+        # binding.pry
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+
+      # binding.pry
+    end
+  end
+
+end
