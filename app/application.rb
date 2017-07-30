3e8c8d8b61933a7d58064f01f8bc9510e5e8195a
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-30 03:39:10 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -15,9 +15,7 @@ class Application
     if req.path.match(/items/)
       req_item = req.path.split("/items/").last
       # binding.pry
-      found_item = self.class.items.find do |item|
-        item.name == req_item
-      end
+      found_item = self.class.items.find { |item| item.name == req_item }
 # binding.pry
       if found_item
         resp.status = 200
@@ -34,6 +32,8 @@ class Application
 
       # binding.pry
     end
+
+    resp.finish
   end
 
 end
