20605964117dc84cb473f4709b72a7a37cc372b5
github username <github email address>
2017-09-18 15:35:58 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
-  @@items = [Item.new("iPhone",$999),
-    Item.new("macbook pro",$1400)]
+  @@items = [Item.new("iPhone",999),
+    Item.new("macbook pro",1400)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find {|i| i.name == item_name}
-      resp.write item.price
+      resp.write "The price of #{item.name} is: $#{item.price}"
       else
         resp.status = 400
         resp.write "Item not found"
