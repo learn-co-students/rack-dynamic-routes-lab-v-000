9689eecfa9befdd6b40f9b4b4c70e9895bb1bc1c
Joanna Sese <jmgsese@gmail.com>
2017-09-18 21:29:43 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  @@items = Array.new
+
+  def call(env)
+
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+# DYNAMIC URL ROUTES
+    if req.path.match(/items/)
+      # REGEX MATCH
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
