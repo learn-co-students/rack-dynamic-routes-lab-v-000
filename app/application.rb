5eeb59a27d7c4449589dd9184c4d8a15a18b2928
github username <github email address>
2017-09-25 01:13:25 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,36 @@
+class Application
+
+  @@items = [Item.new("Mac", 142), Item.new("iPhone", 423)]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/) #/items/Figs
+      item_name = req.path.split("/items/").last #Figs
+      item = @@items.find{|i| i.name == item_name}
+      if item #exists
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
+
+
+#run rackup config.ru to test.
+#visit on chrome: localhost:9292
+
+#localhost:9292
+  #Route note found
+
+#localhost:9292/items
+  #Item not found
+
+#localhost:9292/items/Mac
+  #142
