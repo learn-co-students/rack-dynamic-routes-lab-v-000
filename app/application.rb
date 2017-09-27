cf5c42d8bb767b4c716cde9cf6014bc24d062dc2
daniellecroxton <danikrist@gmail.com>
2017-09-27 00:16:02 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_term = req.params["item"]
+        if @@items.include?(search_term)
+          # @@cart << search_term
+          resp.write "#{item.price}"
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
