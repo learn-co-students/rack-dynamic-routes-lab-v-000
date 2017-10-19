441f143e019fb095bf86e3e30d19eac487b58b8a
GusVilla303 <gusvilla303@gmail.com>
2017-10-18 22:29:28 -0600

    Add Application and Rack request/response, and finders in Item. All specs are passing

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,20 @@
 class Item
+
+  @@items = []
+
   attr_accessor :name, :price
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+
+  def self.all
+    @@items
+  end
+
+  def self.find_by(name)
+    self.all.detect { |item| item.name == name }
   end
 end
