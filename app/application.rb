07d7dfb6fd9adcebf1b9cfda28721e9a8fa59642
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-10-20 23:45:41 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if @@items.any? { |e| e.name == item_name}
+        item_selected = @@items.find { |item| item.name == item_name }
+        resp.write "#{item_selected.price}"
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
