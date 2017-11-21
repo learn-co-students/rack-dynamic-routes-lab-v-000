a8c8edf1a8b019f96db26544f11ebb49ae4e8fcd
Guesly  Germain <germain1211@outlook.com>
2017-11-21 21:28:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,28 @@
-
-# require 'pry'
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    # binding.pry
-    if req.path.match(/item/)
-      @@items.each{|s| resp.write "#{s.price}"}
-
-    elsif
-      resp.write "Route not found"
-      resp.status = 404
-
+    if req.path != "/items"
+    resp.write "Route not found"
+    resp.status = 404
     else
-      resp.write "Item not found"
-      resp.status = 400
+
+       @@items.select do |i|
+      if i.name = req.path[7..-1]
+       i.price
+       binding.pry
+      else
+       resp.write "Item not found"
+       resp.status = 400
+     end
     end
+  end
 
     resp.finish
   end
+
+
 end
