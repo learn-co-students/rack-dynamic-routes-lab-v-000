7291d5ec54e925018f789e489ca073e396d1dab2
Guesly  Germain <germain1211@outlook.com>
2017-11-22 03:31:19 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,24 +5,21 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path != "/items"
-    resp.write "Route not found"
-    resp.status = 404
-    else
+      if req.path[0..6] != "/items/"
+        resp.write "Route not found"
+        resp.status = 404
+        else
 
-       @@items.select do |i|
-      if i.name = req.path[7..-1]
-       i.price
-       binding.pry
-      else
-       resp.write "Item not found"
-       resp.status = 400
-     end
-    end
-  end
+          if item = @@items.find {|i| i.name == req.path[7..-1]}
+           resp.write item.price
+          #  binding.pry
+          else
+           resp.write "Item not found"
+           resp.status = 400
+          end
+
+        end
 
     resp.finish
   end
-
-
 end
