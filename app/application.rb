ad250b475d82cf36d076b6cff5548463d76059b4
Olena Ageyeva <eageeva@hotmail.com>
2017-12-09 01:46:36 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,32 @@
+class Application
+  @@item=[]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = @@items.find{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status=400
+      end
+      else
+      resp.write "Route not found"
+      resp.status=404
+    end
+
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
