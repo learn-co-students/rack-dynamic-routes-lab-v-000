8d60f62cc0b0aaf37acc2d8e6c7c13fcccb58691
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 23:13:18 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+class Application
+
+  @@items = [Item.new("Sorry", "Justin Bieber"),
+            Item.new("Hello","Adele")]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last#turn /items/<Item.name> into <Item.name>
+      if @@items.include?(item_name)
+        item = @@items.find{|s| i.name == item_name}
+        req.return "#{item.price}"
+        #item = @@items.find{|s| i.name == item_name}
+      else
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+      # @@items.each do |item|
+      #   resp.write "#{item.name}\n"
+      # end
+    end
+
+    resp.finish
+  end
+end
