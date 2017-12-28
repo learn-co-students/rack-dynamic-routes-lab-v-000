9bda27369b2e7c7e3f445587cfd9280bc3e73f34
github username <github email address>
2017-12-27 20:16:59 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Application
+
+  @@items = Item.all
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find {|s| s.name == item_name}
+      if @@items.map(&:name).include?(item_name)
+        resp.write "You selected #{item.name} - price: #{item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
