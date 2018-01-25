6365ee865ec535ae95fdad03e265486b045e1dab
Sally Zasloff <szasloff@gmail.com>
2018-01-25 22:11:31 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
 
-  @@items = [Item.new(:name, :price)]
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
     def call(env)
       resp = Rack::Response.new
@@ -19,7 +19,6 @@ class Application
         resp.write "Route not found"
         resp.status = 404
       end
-
       resp.finish
   end
 end
