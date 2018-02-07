ef70f17ed132e2ded56d9922e8734c1c44cadd3f
Richard Schaubeck <richard.schaubeck@gmail.com>
2018-02-07 23:05:21 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+class Application
+
+
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path != "/items"
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      if @@items.include?(item)
+        resp.write item.price
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    end
+
+    resp.finish
+  end
+
+
+end
