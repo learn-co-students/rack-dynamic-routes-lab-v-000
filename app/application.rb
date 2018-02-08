fd5b829c06d4b8b804c369c0355f2df8e271834d
Blakeley Bond <burrick2003@yahoo.com>
2018-02-08 02:56:57 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,18 @@
 class Application
-@@items = []
+@@items = [] #spec fills these
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
+      name = req.path.split("/items/").last
 
-
+        if @@items.find{|element| element.name == name}
+          item = @@items.find{|element| element.name == name}
+          resp.write item.price
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
 
 
     else
