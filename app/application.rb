4f889f4df5116def49dec4dfb54de34d722f598e
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-23 13:17:35 -0500

    Create Application class

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+
+class Application
+
+  @@items = [Item.new("Pears", 0.99),
+             Item.new("Figs", 3.42)]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      entered_item_name = req.path.split("/items/").last
+      if item = @@items.find {|item| item.name == entered_item_name}
+        resp.write item.price
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
