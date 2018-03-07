fa06e161ec512b1327b27a495b50fe0bfc15ff52
thesilentc <thesilentc@me.com>
2018-03-07 13:38:46 -0500

    simplified the @@items array

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,22 @@
 class Application
 
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+@@items = []
 
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      if item =@@items.find{|i| i.name == item_name}
-        resp.write item.price
-      else
-        resp.status = 400
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path.match(/items/)
+  item = req.path.split("/items/").last
+    if item_lookup = @@items.find{|i| i.name == item}
+      resp.write item_lookup.price
+    else resp.status = 400
         resp.write "Item not found"
-      end
-    else
-      resp.status=404
-      resp.write "Route not found"
     end
-    resp.finish
+  else
+      resp.status = 404
+      resp.write "Route not found"
   end
-
+    resp.finish
+end
 end
