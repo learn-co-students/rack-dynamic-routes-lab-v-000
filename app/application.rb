2dcf549eec4663a853f26d936418ae840a6d3b71
Jay Bednar <ironviralmedia@gmail.com>
2018-03-27 23:40:33 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.select{|i| i.name == item_name}.first
+      item = @@items.find{|i| i.name == item_name}
       if item
         resp.write "$#{item.price}"
       else
