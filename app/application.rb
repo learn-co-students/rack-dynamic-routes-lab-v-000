c36b842feb940bfe7eb0fcfb070d11aab24eead3
Dee Race <hey@deeraceart.com>
2018-04-26 15:12:49 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      searched_item = req.path.split("/items/").last
+      if @@items.find {|i| i.name == searched_item}
+      resp.write "#{@@items.find{|i| i.name == searched_item}.price}"
+    else
+      resp.write "Item not found"
+      resp.status = 400
+    end
+
+  else
+    resp.write "Route not found"
+    resp.status = 404
+  end
+
+    resp.finish
+  end
+end
