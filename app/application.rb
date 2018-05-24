38789f644703e53e869119b156a730663fac49f5
eyi1 <ej7729@gmail.com>
2018-05-24 11:26:22 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
         req = Rack::Request.new(env)
         
         if req.path.match(/items/)
-            item_name = req.path.split("/items/").last     
+            item_name = req.path.split("/items/").first     
             
             if item = @@items.find{|i| i.name == item_name} 
                 resp.write item.price
