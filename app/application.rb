15372521e2a89373a2b8632ae802ef6c2a432704
Emily S <emily.sugano@gmail.com>
2018-06-26 22:24:09 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp =Rack::Response.new
+    req = Rack::Response.new(env)
+
+    #if /items/<ITEM NAME> route
+    if req.path.match("/items/")
+      #get item name
+      item_name = req.path.split("/items/").last
+      #see if item exists
+      item_find = Item.all.find{|i| i.name == item_name}
+
+      #if items exists, return its price
+      if req.path.match("/items/#{item_name}")
+      #if item doesn't exists, return a 400 page
+      # else
+      #   resp.status = 400
+      #   resp.write "Item not found"
+      # end
+    #all other paths are 404
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+
+  end #ends #calls
+end #ends class
