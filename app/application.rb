429989667f584de3a56dc2acd7398807a143d01e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 15:35:14 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,11 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+  # this is a bit more elegant:
+  # if req.path.match(/items/)
+  #   item_name = req.path.split("/items/").last
+  #   if item =@@items.find{|i| i.name == item_name}
+  #     resp.write item.price
     if req.path.match(/items/) != nil
       req_item = req.path.match(/(?<=items\/).*/).to_s
       found_item = @@items.select {|i| i.name == req_item}
