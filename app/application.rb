d486f67d6035ebb3fff70acb4993ce992ea88877
Dani Schuhman <dani8439@aol.com>
2018-08-15 17:54:57 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,10 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last #tuns /items/Item into Item
+      item_name = req.path.split("/items/").last #turn /items/Pears into Pears
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
+        resp.status = 200
       else
         resp.write "Item not found"
         resp.status = 400
