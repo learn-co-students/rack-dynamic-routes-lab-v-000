826ae4091eb0e859800f52e04f40102efc01ab10
Wasif Khan <waasif.khaan@gmail.com>
2018-08-17 02:36:36 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application 
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+      search_term = req.params["Item_name"]
+      @@items.each do |item|
+        resp.write item.price
+      end 
+      resp.status = 200
+    else 
+      resp.write "Item not found"
+      resp.status = 400
+    end 
+    if !req.path.match(/items/)
+      resp.status = 4004
+      resp.write "Route not found"
+    end 
+     
+  end 
+
+end 
