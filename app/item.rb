61baa5e7caa3522226401765314a0c8c50abcccd
Harley Rayner <harleymteden@gmail.com>
2018-11-20 19:02:07 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,17 @@
 class Item
+
+  @@items = []
+
   attr_accessor :name, :price
 
   def initialize(name,price)
     @name = name
     @price = price
   end
+
+  def self.all
+    @@items
+  end
+
+
 end
