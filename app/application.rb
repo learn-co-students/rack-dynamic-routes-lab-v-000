44774814b30685574d2ba43589abebaaf4a21f8b
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-12-10 09:13:35 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       if @@items.include?(item)
-        resp.write "#{item.price}"
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
