e69a562d54e72e484b9c63510a4d022b67979e60
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-12-10 09:12:49 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,6 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      #searched_item = req.params["q"]
-      #binding.pry
       if @@items.include?(item)
         resp.write "#{item.price}"
       else
