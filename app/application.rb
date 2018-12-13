4aa1795cc62400493040a463810ad1c731e29943
Jason Mei <jason.mei403@gmail.com>
2018-12-13 04:52:42 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application < Item
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = Item.all.find { |i| i.name == item_name }
+      resp.write item.price
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+
+end
