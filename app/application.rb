65f0a01c4ffffc3f12ef7c96858e2c3e5356f1fc
Brad Schrag <brad.schrag@gmail.com>
2019-01-11 22:24:06 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split('/items/').last
+      new_item = Item.return_match_by_name(item_name)
+      if new_item
+        resp.write "#{new_item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    resp.finish
+  end
+  
+end
