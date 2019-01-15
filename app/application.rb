d50e4b7943ba5f100bc73e5bf007c11dd8258611
Azhar Shah <azharshahstl@yahoo.com>
2019-01-15 17:31:26 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,12 +9,13 @@ class Application
       
       item_name = req.path.split("/items/").last
       
-      if item = @@items.find{|i| item.name == item_name}
+      item = @@items.find{|i| item.name == item_name}
+      binding.pry
         resp.write item.price
-      else
+      
         resp.write "Item not found" 
         resp.status = 400
-      end
+      
     else 
       resp.write "Route not found" 
       resp.status = 404 
