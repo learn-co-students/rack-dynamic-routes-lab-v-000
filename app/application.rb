ad470da932f406f23e411fa3ba8f24c5b0373bfa
Allison Howard <alliemhow@gmail.com>
2019-01-21 18:20:04 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,5 @@
 class Application
 
-
   def call (env)
     resp = Rack::Response.new
     req = Rack::Request.new (env)
@@ -17,6 +16,7 @@ class Application
       resp.write "Route not found"
       resp.status=404
     end
+
     resp.finish
   end
 end
