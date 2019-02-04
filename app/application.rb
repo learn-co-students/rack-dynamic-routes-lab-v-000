289ace27428a653b11c97373b1add8adff2c60ca
Elias Ventura <eliasventura23@gmail.com>
2019-02-04 16:39:11 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+class Application
+
+@@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_to_lookup = req.path.split("/items/").last
+      item = @@items.find {|i| i.name == item_to_lookup}
+
+      if item
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write 'Item not found'
+
+      end
+
+
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+
+    resp.finish
+  end
+
+end
