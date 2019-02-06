e0cf0ca9b6397f841cf966fb7f0b508efcb3845b
Ashley Wong <ashwong04@gmail.com>
2019-02-06 15:37:49 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,18 +6,18 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items"
+    if req.path.match(/items/)
       search_term = req.params["item"]
         if @@items.include?(search_term)
           resp.write "#{search_term.price}"
         else
           resp.write "Item not found"
-          resp.status 400
+          resp.status = 400
         end
 
     else
       resp.write "Route not found"
-      resp.status 404
+      resp.status = 404
     end
 
     resp.finish
