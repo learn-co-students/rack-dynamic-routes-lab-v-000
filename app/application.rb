47d0a9d8191d1c13eb9e12c805b876b2a92143d3
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-31 16:03:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,26 +1,22 @@
 class Application
-
   @@items = []
+  #@@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)] # For web testing purposes
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    resp = Rack::Response.new # Instance of the HTTP response
+    req = Rack::Request.new(env) # Instance of the HTTP request
+    # Requests have a `path` which is basically whatever is after the url
 
-    if req.path.match(/items/)
-	  # route has items
-      item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
-
-      if item
-		#item does exitst
+    if req.path.match(/items/) # checks if path has items in it :ie localhost:9292/items/Apples
+      item_name = req.path.split("/items/").last # splits the path to get whatever is after /items/ ie: `Apples`
+      item = @@items.find{|i| i.name == item_name} # tries to find said item in a the items list
+      if item #Returns item price if it is in @@item. If the item is not nil
         resp.write item.price
-      else
-		#item does not exists
+      else #Returns an error and status of 400 if the item is not there
         resp.write "Item not found"
         resp.status = 400
       end
-    else
-      # route has no items
+    else # Returns 404 for a bad route
       resp.write "Route not found"
       resp.status = 404
     end
