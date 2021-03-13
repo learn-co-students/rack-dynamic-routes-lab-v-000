53f8d18927fe1c5ed452a34d72947608f3e61f2a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-13 03:23:25 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+
+    if req.path.match(/items/)
+      search_item = req.path.split("/items/").last
+      item = @@items.find {|item| item.name == search_item}
+
+      if item
+        resp.write item.price
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+
+  end
+
+end
